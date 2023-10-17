b84f8d8066417498ce74b8a108c769c920359ba3
DANG DAI DUONG <127579534+gunzosi@users.noreply.github.com>
2023-10-17 20:10:20 +0700

    Add files via upload

---
+++ C02_ControlStatement/IfStatementDemo.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP if Statement Demo</title>
+</head>
+<body>
+    <?php $is_admin = true; ?>
+    <?php if ( $is_admin ) : ?>
+        <a href="#">In IF Statement and show when condition is true <br></a>
+    <?php endif; ?>
+    <a href="#">Out of IF Statement</a>
+</body>
+</html>
